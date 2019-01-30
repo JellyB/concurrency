74790f56a56a8f44dd44e466e2fe8c2061514d0a
jellyb <jelly_b@126.com>
2019-01-30 15:16:16 +0800

    use different object

--- src/main/java/com/mmall/concurrency/example/sync/bigd/SynchronizedCodeBlockAndMethod.java
+++ src/main/java/com/mmall/concurrency/example/sync/bigd/SynchronizedCodeBlockAndMethod.java
@@ -16,10 +16,10 @@ public class SynchronizedCodeBlockAndMethod {
     /**
      * synchronized 修饰一个代码块
      */
-    private void synCodeBlock(){
+    private void synCodeBlock(String object){
         synchronized (this){
             for (int i = 0; i < 10; i ++){
-                log.info("synCodeBlock current value:{}", i);
+                log.info("synCodeBlock current value:{}, >>> current object:{}", i, object);
             }
         }
     }
@@ -34,13 +34,13 @@ public class SynchronizedCodeBlockAndMethod {
     }
 
     public static void main(String[] args) {
-        SynchronizedCodeBlockAndMethod codeBlock = new SynchronizedCodeBlockAndMethod();
-        SynchronizedCodeBlockAndMethod method = new SynchronizedCodeBlockAndMethod();
+        SynchronizedCodeBlockAndMethod synchronizedCodeBlockAndMethod1 = new SynchronizedCodeBlockAndMethod();
+        SynchronizedCodeBlockAndMethod synchronizedCodeBlockAndMethod2 = new SynchronizedCodeBlockAndMethod();
         ExecutorService pool =  new ThreadPoolExecutor(10, 20, 10, TimeUnit.SECONDS, new LinkedBlockingDeque<>(30));
-        //pool.execute( () -> codeBlock.synCodeBlock());
-        //pool.execute( () -> codeBlock.synCodeBlock());
-        pool.execute( () -> method.syncMethod());
-        pool.execute( () -> method.syncMethod());
+        pool.execute( () -> synchronizedCodeBlockAndMethod1.synCodeBlock("object 1"));
+        pool.execute( () -> synchronizedCodeBlockAndMethod2.synCodeBlock("object 2"));
+        //pool.execute( () -> method.syncMethod());
+        //pool.execute( () -> method.syncMethod());
 
     }
 }
