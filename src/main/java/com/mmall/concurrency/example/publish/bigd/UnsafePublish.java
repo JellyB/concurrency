a95c36edf5124620ff0bb7c3a01a5ebb3c390b7b
jellyb <jelly_b@126.com>
2019-03-01 22:45:50 +0800

    不安全的发布对象

---
+++ src/main/java/com/mmall/concurrency/example/publish/bigd/UnsafePublish.java
@@ -0,0 +1,29 @@
+package com.mmall.concurrency.example.publish.bigd;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Arrays;
+
+/**
+ * 描述：
+ *
+ * @author biguodong
+ * Create time 2019-03-01 10:39 PM
+ **/
+@Slf4j
+public class UnsafePublish {
+
+    private String[] status = {"a", "b", "c"};
+
+    public String[] getStatus(){
+        return status;
+    }
+
+    public static void main(String[] args) {
+        UnsafePublish unsafePublish = new UnsafePublish();
+        log.info("states:{}", Arrays.toString(unsafePublish.getStatus()));
+
+        unsafePublish.getStatus()[0] = "d";
+        log.info("states:{}",  Arrays.toString(unsafePublish.getStatus()));
+    }
+}
