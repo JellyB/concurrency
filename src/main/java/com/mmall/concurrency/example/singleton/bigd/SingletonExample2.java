bd27ed3360f99c13c0fab82a3fd5e1f5970d479c
jellyb <jelly_b@126.com>
2019-03-02 13:28:05 +0800

    饿汉模式实现单例模式

---
+++ src/main/java/com/mmall/concurrency/example/singleton/bigd/SingletonExample2.java
@@ -0,0 +1,27 @@
+package com.mmall.concurrency.example.singleton.bigd;
+
+import com.mmall.concurrency.annoations.ThreadSafe;
+
+/**
+ * 描述：饿汉模式实现单例模式
+ * 饿汉模式在类装载的时候创建
+ *
+ * @author biguodong
+ * Create time 2019-03-02 1:20 PM
+ **/
+@ThreadSafe
+public class SingletonExample2 {
+
+    /**
+     * 私有构造函数
+     */
+    private SingletonExample2(){
+
+    }
+ 
+    private static SingletonExample2 instance = new SingletonExample2();
+
+    public static SingletonExample2 getInstance(){
+        return instance;
+    }
+}
