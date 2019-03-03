eb25130437ab978bb5b1d7bfa4ab70d60a947982
jellyb <jelly_b@126.com>
2019-03-03 23:18:35 +0800

    有问题的双重同步锁单例模式

--- src/main/java/com/mmall/concurrency/example/singleton/bigd/SingletonExample4.java
+++ src/main/java/com/mmall/concurrency/example/singleton/bigd/SingletonExample4.java
@@ -18,11 +18,23 @@ public class SingletonExample4 {
     private SingletonExample4(){
 
     }
-    
 
     /**
-     * 单例对象
+     * 1. memory = allocate() 分配对象的内存空间；
+     * 2. ctorInstance() 初始化对象；
+     * 3. instance = memory 设置 instance 指向刚分配的n内存
+     *
      */
+
+    // JVM 和 CPU 优化发生了指令重拍
+
+    /**
+     * 1. memory = allocate() 分配对象的内存空间；
+     * 3. instance = memory 设置 instance 指向刚分配的n内存
+     * 2. ctorInstance() 初始化对象；
+     *
+     */
+
     private static SingletonExample4 instance = null;
 
     /**
@@ -30,10 +42,10 @@ public class SingletonExample4 {
      * @return
      */
     public static SingletonExample4 getInstance(){
-        if(null == instance){ // 双重检测机制
+        if(null == instance){ // 双重检测机制     // 线程 B
             synchronized (SingletonExample4.class){ // 同步锁
                 if(null == instance){
-                    instance = new SingletonExample4();
+                    instance = new SingletonExample4(); // 线程 A 执行到了指令重排后的第 3 步
                 }
             }
         }
